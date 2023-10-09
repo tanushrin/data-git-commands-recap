0cb525a3c9a094e6cd266b7f4f9b052038f413ba
Tanushri Nayak <tanushri0310003@gmail.com>
2023-10-09 16:34:28 +0200

    Fix style issues, should get a 'Good Style now.

--- today.py
+++ today.py
@@ -1,11 +1,12 @@
-'''
-This is the module docstring
-'''
+"""A module computing my git name for the day"""
+
 
 def my_name_is():
+    """my_name_is is to compute my git name"""
     return "tanushrin"
 #pass  # YOUR CODE HERE
 
 
 def my_buddy_is():
+    """my_buddy_is is to compute my buddy's git name"""
     return "DavisPC88088"
